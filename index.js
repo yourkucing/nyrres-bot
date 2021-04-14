0ff2bf035f63d3fc91e515113f8ea4bf68376b70
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-04-14 22:52:25 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({disableEveryone: false});
+const prefix = "uwu ";
+const fs = require('fs').promises;
+const path = require('path');
+
+
+client.commands = new Map();
+
+client.on('ready', () => {
+ console.log(`Logged in as ${client.user.tag}!`);
+ client.user.setActivity('uwu help', { type: 'STREAMING' });
+ });
+
+client.on('message', msg => {
+    if (msg.author.bot) return
+
+        if (msg.content.toLowerCase() === 'hi nyrres') {
+            if (msg.author.bot) return;
+            else {
+                msg.channel.send('hihi!');
+            }
+        }
+    });
+
+client.login(process.env.token1);

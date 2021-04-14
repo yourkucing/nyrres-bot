c56e4b3bd43bbb8dfc9263bed58c5962ca45167d
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-04-15 04:57:16 +0800

    Update createProfile.js

--- commands/createProfile.js
+++ commands/createProfile.js
@@ -1,39 +1,35 @@
 const Discord = require('discord.js');
 const profileModel = require('../models/profileSchema');
 
-// module.exports.run = async(client, msg) =>{
-//     msg.channel.send(`Greetings, ${msg.guild.members.cache.get(msg.author.id).displayName}. Please key in your (character name, race, class): ("exit" to exit.)\nExample: Athur Thorac, Human, Rogue`);
-//     msg.channel.awaitMessages(m => m.author.id == msg.author.id, {max: 1, time: 50000}).then(collected => {
-//         if (collected.first().content.toLowerCase() == 'exit') {
-//             msg.channel.send("Goodbye for now!");
-//         }
-//         else {
-//             nameInput = collected.first().content;
-//             msg.channel.send(`Please key in your class: ("exit" to exit.)`);
-//             msg.channel.awaitMessages(m => m.author.id == msg.author.id, {max: 1, time: 50000}).then(collected => {
-//                 if (collected.first().content.toLowerCase() == 'exit') {
-//                     msg.channel.send("Goodbye for now!");
-//                 }
-//                 else {
-//                     classInput = collected.first().content;
-//                     let profile = await profileModel.create({
-//                         userID = msg.author.id,
-//                         serverID = msg.author.guild.id,
-//                         characterName = nameInput,
-//                         className = classInput,
-//                         race = None
-//                     });
-//                     profile.save();
-//                 }
-//             }).catch(collected => {
-//                 msg.channel.send('No reply after 50 seconds, profile creation cancelled!')
-//             })
-//         }
-//         }).catch(collected => {
-//             msg.channel.send('No reply after 50 seconds, profile creation cancelled!');
-//         });
-// }
-
-module.exports.run = async(client, msg, args) => {
-	msg.channel.send("hihi");
+module.exports.run = async(client, msg) =>{
+    msg.channel.send(`Greetings, ${msg.guild.members.cache.get(msg.author.id).displayName}. Please key in your (character name, race, class): ("exit" to exit.)\nExample: Athur Thorac, Human, Rogue`);
+    msg.channel.awaitMessages(m => m.author.id == msg.author.id, {max: 1, time: 50000}).then(collected => {
+        if (collected.first().content.toLowerCase() == 'exit') {
+            msg.channel.send("Goodbye for now!");
+        }
+        else {
+            nameInput = collected.first().content;
+            msg.channel.send(`Please key in your class: ("exit" to exit.)`);
+            msg.channel.awaitMessages(m => m.author.id == msg.author.id, {max: 1, time: 50000}).then(collected => {
+                if (collected.first().content.toLowerCase() == 'exit') {
+                    msg.channel.send("Goodbye for now!");
+                }
+                else {
+                    classInput = collected.first().content;
+                    let profile = await profileModel.create({
+                        userID = msg.author.id,
+                        serverID = msg.author.guild.id,
+                        characterName = nameInput,
+                        className = classInput,
+                        race = None
+                    });
+                    profile.save();
+                }
+            }).catch(collected => {
+                msg.channel.send('No reply after 50 seconds, profile creation cancelled!')
+            })
+        }
+        }).catch(collected => {
+            msg.channel.send('No reply after 50 seconds, profile creation cancelled!');
+        });
 }

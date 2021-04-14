a556a91eef543ebced2dfe6e38ea2bbef8283754
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-04-15 06:19:01 +0800

    changes

--- commands/character.js
+++ commands/character.js
@@ -1,6 +1,5 @@
 const profileModel = require('../models/profileSchema');
 const Discord = require('discord.js');
-const { profile } = require('node:console');
 
 module.exports.run = async(client, msg, profileData) => {
     msg.channel.send(`Your character name is ${profileData.characterName}.`)

207fbedd9eee5fe57a60a9f1b3a271bc1687f7cd
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-04-15 01:28:04 +0800

    changes

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const mongoose = require('mongoose');
 
 client.commands = new Map();
 
-
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     client.user.setActivity('ny help', { type: 'STREAMING' });

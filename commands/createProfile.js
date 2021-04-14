ab8222baeef9205640ab1e45a2909b43f3587771
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-04-15 05:26:13 +0800

    Update createProfile.js

--- commands/createProfile.js
+++ commands/createProfile.js
@@ -2,32 +2,31 @@ const Discord = require('discord.js');
 const profileModel = require('../models/profileSchema');
 
 module.exports.run = async(client, msg) =>{
-	human = msg.author.id
-	guildhuman = msg.author.guild.id
-    msg.channel.send(`Greetings, ${msg.guild.members.cache.get(msg.author.id).displayName}. Please key in your character name: ("exit" to exit.)`);
-    msg.channel.awaitMessages(m => m.author.id == msg.author.id, {max: 1}).then(collected => {
-        if (collected.first().content.toLowerCase() == 'exit') {
-            msg.channel.send("Goodbye for now!");
-        }
-        else {
-            nameInput = collected.first().content;
-            msg.channel.send(`Please key in your class: ("exit" to exit.)`);
-            msg.channel.awaitMessages(m => m.author.id == msg.author.id, {max: 1}).then(collected => {
-                if (collected.first().content.toLowerCase() == 'exit') {
-                    msg.channel.send("Goodbye for now!");
-                }
-                else {
-                    classInput = collected.first().content;
-                    let profile = profileModel.create({
-                        userID: human,
-                        serverID: guildhuman,
-                        characterName: nameInput,
-                        className: classInput,
-                        race: null
-                    });
-                    profile.save();
-                }
-            })
-        }
-        });
+	msg.channel.send(msg.author);
+    // msg.channel.send(`Greetings, ${msg.guild.members.cache.get(msg.author.id).displayName}. Please key in your character name: ("exit" to exit.)`);
+    // msg.channel.awaitMessages(m => m.author.id == msg.author.id, {max: 1}).then(collected => {
+    //     if (collected.first().content.toLowerCase() == 'exit') {
+    //         msg.channel.send("Goodbye for now!");
+    //     }
+    //     else {
+    //         nameInput = collected.first().content;
+    //         msg.channel.send(`Please key in your class: ("exit" to exit.)`);
+    //         msg.channel.awaitMessages(m => m.author.id == msg.author.id, {max: 1}).then(collected => {
+    //             if (collected.first().content.toLowerCase() == 'exit') {
+    //                 msg.channel.send("Goodbye for now!");
+    //             }
+    //             else {
+    //                 classInput = collected.first().content;
+    //                 let profile = profileModel.create({
+    //                     userID: human,
+    //                     serverID: guildhuman,
+    //                     characterName: nameInput,
+    //                     className: classInput,
+    //                     race: null
+    //                 });
+    //                 profile.save();
+    //             }
+    //         })
+    //     }
+    //     });
 }

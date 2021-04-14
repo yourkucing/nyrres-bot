a6d8eebd87766beb48135185055c52660aafa280
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-04-15 05:11:09 +0800

    Update createProfile.js

--- commands/createProfile.js
+++ commands/createProfile.js
@@ -3,14 +3,14 @@ const profileModel = require('../models/profileSchema');
 
 module.exports.run = async(client, msg) =>{
     msg.channel.send(`Greetings, ${msg.guild.members.cache.get(msg.author.id).displayName}. Please key in your (character name, race, class): ("exit" to exit.)\nExample: Athur Thorac, Human, Rogue`);
-    msg.channel.awaitMessages(m => m.author.id == msg.author.id, {max: 1, time: 50000}).then(collected => {
+    msg.channel.awaitMessages(m => m.author.id == msg.author.id, {max: 1}).then(collected => {
         if (collected.first().content.toLowerCase() == 'exit') {
             msg.channel.send("Goodbye for now!");
         }
         else {
             nameInput = collected.first().content;
             msg.channel.send(`Please key in your class: ("exit" to exit.)`);
-            msg.channel.awaitMessages(m => m.author.id == msg.author.id, {max: 1, time: 50000}).then(collected => {
+            msg.channel.awaitMessages(m => m.author.id == msg.author.id, {max: 1}).then(collected => {
                 if (collected.first().content.toLowerCase() == 'exit') {
                     msg.channel.send("Goodbye for now!");
                 }
@@ -25,11 +25,7 @@ module.exports.run = async(client, msg) =>{
                     });
                     profile.save();
                 }
-            }).catch(collected => {
-                msg.channel.send('No reply after 50 seconds, profile creation cancelled!')
             })
         }
-        }).catch(collected => {
-            msg.channel.send('No reply after 50 seconds, profile creation cancelled!');
         });
 }

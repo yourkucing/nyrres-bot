70b15e1a7b6cc1d85ac6031ee29ef33f1849b8a2
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-04-15 18:15:06 +0800

    Update whoami.js

--- commands/whoami.js
+++ commands/whoami.js
@@ -55,7 +55,7 @@ module.exports.run = async(client, msg, args) => {
             cond.push("Exhaustion")
         }
 
-        if (cond.length < 0) {
+        if (cond.length < 1) {
             cond.push("You have no conditions currently.")
         }
 

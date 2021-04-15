4502a2000ec44c9aad7221ea1b5e3ad8af13b5ab
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-04-15 20:35:04 +0800

    Update race.js

--- commands/information/race.js
+++ commands/information/race.js
@@ -14,23 +14,7 @@ module.exports.run = async(client, msg, args) => {
     \nAlignment: Dragonborn tend to extremes, making a conscious choice for one side or the other in the cosmic war between good and evil (represented by Bahamut and Tiamat, respectively). Most dragonborn are good, but those who side with Tiamat can be terrible villains.
     \nSize: Dragonborn are taller and heavier than humans, standing well over 6 feet tall and averaging almost 250 pounds. Your size is Medium.
     \nSpeed: Your base walking speed is 30 feet.
-    \nDraconic Ancestry: You have draconic ancestry. Choose one type of dragon from the Draconic Ancestry table. Your breath weapon and damage resistance are determined by the dragon type, as shown in the table.
-    \n
-    ┌────────┬─────────────┬──────────────────────────────┐
-    │ Dragon │ Damage Type │        Breath Weapon         │
-    ├────────┼─────────────┼──────────────────────────────┤
-    │ Black  │ Acid        │ 5 by 30 ft. line (Dex. save) │
-    │ Blue   │ Lightning   │ 5 by 30 ft. line (Dex. save) │
-    │ Brass  │ Fire        │ 5 by 30 ft. line (Dex. save) │
-    │ Bronze │ Lightning   │ 5 by 30 ft. line (Dex. save) │
-    │ Copper │ Acid        │ 5 by 30 ft. line (Dex. save) │
-    │ Gold   │ Fire        │ 15 ft. cone (Dex. save)      │
-    │ Green  │ Poison      │ 15 ft. cone (Con. save)      │
-    │ Red    │ Fire        │ 15 ft. cone (Dex. save)      │
-    │ Silver │ Cold        │ 15 ft. cone (Con. save)      │
-    │ White  │ Cold        │ 15 ft. cone (Con. save)      │
-    └────────┴─────────────┴──────────────────────────────┘
-    
+    \nDraconic Ancestry: You have draconic ancestry. Choose one type of dragon from the Draconic Ancestry table. Your breath weapon and damage resistance are determined by the dragon type, as shown in the table. 
     \nBreath Weapon: You can use your action to exhale destructive energy. Your draconic ancestry determines the size, shape, and damage type of the exhalation. When you use your breath weapon, each creature in the area of the exhalation must make a saving throw, the type of which is determined by your draconic ancestry. The DC for this saving throw equals 8 + your Constitution modifier + your proficiency bonus. A creature takes 2d6 damage on a failed save, and half as much damage on a successful one. The damage increases to 3d6 at 6th level, 4d6 at 11th level, and 5d6 at 16th level. After you use your breath weapon, you can’t use it again until you complete a short or long rest.
     \nDamage Resistance: You have resistance to the damage type associated with your draconic ancestry.
     \nLanguages: You can speak, read, and write Common and Draconic. Draconic is thought to be one of the oldest languages and is often used in the study of magic. The language sounds harsh to most other creatures and includes numerous hard consonants and sibilants.`},

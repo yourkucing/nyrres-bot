4d320fb8b876359c8ccfe0a90247d65ad17f7137
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-04-15 14:47:21 +0800

    Update character.js

--- commands/character.js
+++ commands/character.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 module.exports.run = async(client, msg) => {
 	let hooman = msg.author.id
 	let guild = msg.guild.id
-    profileData = profileModel.findOne({ userID: '279101053750870017'})
+    profileData = profileModel.find({ userID: '279101053750870017'})
     if(!profileData){
         msg.channel.send(`Sorry ${msg.author}, you don't have a character! You can create one using __ny profileCreate__!`)
     }

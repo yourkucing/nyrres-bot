e81e01ebce2210e34f25eba4af5f4be44231446e
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-04-22 21:29:21 +0800

    Update delete.js

--- commands/delete.js
+++ commands/delete.js
@@ -4,17 +4,11 @@ const Discord = require('discord.js');
 
 module.exports.run = async(client, msg, args) => {
     let hooman = msg.author.id
+    u = ""
     msg.channel.send(`Are you sure you want to delete? (Answer yes or no. If not answered within 30 seconds, I will ignore it.)`)
     msg.channel.awaitMessages(m => m.author.id == msg.author.id, {max: 1, time: 30000}).then(collected => {
         if (collected.first().content.toLowerCase() == 'yes') {
-            deleteData = profileModel.deleteOne({userID: hooman})
-            if (!deleteData) {
-                msg.channel.send(`Sorry ${msg.author}, you don't have a character!`)
-            }
-            else {
-                deleteMoney = moneyModel.deleteOne({userID: hooman})
-                msg.channel.send(`Character deleted!`)
-            }
+            u = "yes"
         }
         else {
             msg.channel.send(`Oh, no deletion occurred then!`)
@@ -22,5 +16,16 @@ module.exports.run = async(client, msg, args) => {
     }).catch(collected => {
         msg.channel.send('Oh, I guess it was an accident then. No deletion occurred, have a nice day!');
     });
+
+    if (u == "yes") {
+        deleteData = await profileModel.deleteOne({userID: hooman})
+        if (!deleteData) {
+            msg.channel.send(`Sorry ${msg.author}, you don't have a character!`)
+        }
+        else {
+            deleteMoney = await moneyModel.deleteOne({userID: hooman})
+            msg.channel.send(`Character deleted!`)
+        }
+    }
 
 }

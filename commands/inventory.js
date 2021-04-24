1113bce7e76384a9efb4124b0a422f186e3137a3
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-04-25 00:34:56 +0800

    changes

--- commands/inventory.js
+++ commands/inventory.js
@@ -5,39 +5,38 @@ const Discord = require('discord.js');
 module.exports.run = async(client, msg, args) => {
     let hooman = msg.author.id
     profileData = await profileModel.findOne({userID: hooman})
-    inventoryData = await inventoryModel.findOne({userID: hooman})
-    if (!profileData || !inventoryData) {
+    if (!profileData) {
         msg.channel.send(`Sorry ${msg.author}, you don't have a character! You can create one using **ny create**.`)
     }
     else {
-        equipment = inventoryData.equipment
-        weapon = inventoryData.weapon
-        mount = inventoryData.mount
-        pet = inventoryData.pet
+        weapon = await inventoryModel.find({userID: hooman}, {category: 'weapon'})
+        equipment = await inventoryModel.find({userID: hooman}, {category: 'equipment'})
+        mount = await inventoryModel.find({userID: hooman}, {category: 'mount'})
+        pet = await inventoryModel.find({userID: hooman}, {category: 'pet'})
 
-        if (equipment == null) {
-            equipment = 'No equipments.'
-        }
-        if (weapon == null) {
+        if (!weapon) {
             weapon = 'No weapons.'
         }
-        if (mount == null) {
+        if (!equipment) {
+            equipment = 'No equipments.'
+        }
+        if (!mount) {
             mount = 'No mounts.'
         }
-        if (pet == null) {
+        if (!pet) {
             pet = 'No pets.'
         }
 
         url = `https://cdn.discordapp.com/avatars/${hooman}/${msg.author.avatar}.png`
         const embed = new Discord.MessageEmbed()
 		.setColor('#FF69B4')
-		.setTitle(`**${inventoryData.characterName}'s** Inventory`)
+		.setTitle(`**${profileData.characterName}'s** Inventory`)
 		.setDescription('You can see your money here.')
 		.addFields(
-		{ name: `Weapons`, value: `${weapon}`},
+		{ name: `Weapons`, value: `${weapon}`, inline: true},
         { name: `Equipments`, value: `${equipment}`, inline: true},
         { name: `Mounts`, value: `${mount}`},
-        { name: `Pets`, value: `${pet}`, inline: true}
+        { name: `Pets`, value: `${pet}`}
 		)
         .setThumbnail(`${url}`);
 		msg.channel.send(embed);

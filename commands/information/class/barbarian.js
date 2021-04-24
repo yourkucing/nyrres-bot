cd4ff954057e685153232549e8f044eb0a1e18ae
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-04-24 14:32:20 +0800

    Update barbarian.js

--- commands/information/class/barbarian.js
+++ commands/information/class/barbarian.js
@@ -134,11 +134,11 @@ module.exports.run = async(client, msg, args) => {
                     **Mindless Rage**
                     Beginning at 6th level, you can’t be charmed or frightened while raging. If you are charmed or frightened when you enter your rage, the effect is suspended for the duration of the rage`},
                     { name: `Path of the Berserker Part 2`, value: `**Intimidating Presence**
-                    Beginning at 10th level, you can use your action to frighten someone with your menacing presence. When you do so, choose one creature that you can see within 30 feet of you. If the creature can see or hear you, it must succeed on a Wisdom saving throw (DC equal to 8 + your proficiency bonus + your Charisma modifier) or be frightened of you until the end of your next turn. On subsequent turns, you can use your action to extend the duration of this effect on the frightened creature until the end of your next turn. This effect ends if the creature ends its turn out of line of sight or more than 60 feet away from you.
-                    If the creature succeeds on its saving throw, you can’t use this feature on that creature again for 24 hours.
+                    Beginning at 10th level, you can use an action to frighten someone with your menacing presence and choose a creature that you can see within 30 feet of you. If the creature can see/hear you, it must succeed on a Wisdom saving throw (DC = 8 + Proficiency + Charisma) or be frightened of you until the end of your next turn. On next turns, you can use your action to extend the duration of this effect on the frightened creature until the end of your next turn. This effect ends if the creature ends its turn out of line of sight or more than 60 feet away from you.
+                    If the creature succeeds on its saving throw, you can’t use this feature on that creature again for 24 hrs.
                     
                     **Retaliation**
-                    Starting at 14th level, when you take damage from a creature that is within 5 feet of you, you can use your reaction to make a melee weapon attack against that creature.`})
+                    Starting at 14th level, when you take damage from a creature that is within 5 ft of you, you can use your reaction to make a melee weapon attack against that creature.`})
                 embed.setFooter(`Page ${page} of 2. More information: https://www.dndbeyond.com/classes/barbarian`);
                 msg.edit(embed)
                 msg.reactions.resolve('⏩').users.remove(msg.author.id);

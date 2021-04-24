94c36be5555a197ab4640fd4a5e75f620d3cb5ef
Maryam Dinzly <thatdinzlygirl@gmail.com>
2021-04-25 01:32:38 +0800

    Create insert.js

---
+++ commands/dmcommands/insert.js
@@ -0,0 +1,26 @@
+const profileModel = require('../models/profileSchema');
+const inventoryModel = require('../models/inventorySchema');
+const Discord = require('discord.js');
+
+module.exports.run = async(client, msg, args) => {
+    let hooman = msg.author.id
+    let guild = msg.guild.id
+    stuffs = args.split(',')
+    stuffs[0] = stuffs[0].trim()
+    stuffs[1] = stuffs[1].trim()
+
+    profileData = await profileModel.findOne({userID: hooman})
+    let inventory;
+        try {
+            inventory = inventoryModel.create({
+                userID: hooman,
+                serverID: guild,
+                characterName: profileData.characterName,
+                item: stuffs[0],
+                category: stuffs[1]
+            });
+            msg.channel.send(`Added!`)
+        } catch (err) {
+            console.log(err)
+        }
+}

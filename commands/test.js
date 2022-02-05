5b51a67f92433999c993eb763d939eb893ae9ca5
Maryam Dinzly <thatdinzlygirl@gmail.com>
2022-02-05 23:24:18 +0800

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -9,5 +9,14 @@ module.exports.run = async(client, msg, args) => {
     item = args.join(" ")
     msg.channel.send(item)
     let res = await xiv.search(item)
-    console.log(res)
+    const embed = new MessageEmbed()
+    .setColor('#FF69B4')
+    .setTitle(`**Results for ${item}:**`)
+    .setDescription(`Found: ${res.pagination.results_total}`)
+    .setThumbnail(res.results.icon)
+    .addFields(
+        { name: res.results.name, value: res.results.url}
+    )
+    .setFooter(`Page ${res.pagination.page} of ${res.pagination.page_total}`);
+    msg.channel.send({embeds: [embed]});
 }

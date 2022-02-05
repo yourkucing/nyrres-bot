8daf826c6706cf023017833a104ea933e0b6e338
Maryam Dinzly <thatdinzlygirl@gmail.com>
2022-02-05 23:43:32 +0800

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -8,6 +8,9 @@ const xiv = new XIVAPI({
 module.exports.run = async(client, msg, args) => {
     item = args.join(" ")
     let res = await xiv.search(item)
+    fetch(`https://xivapi.com${res.results[0].url}`, { mode: 'cors' })
+	.then(response => response.json())
+	.then(data => console.log(data.Name_en))
     const embed = new MessageEmbed()
     .setColor('#FF69B4')
     .setTitle(`**Results for ${item}:**`)
